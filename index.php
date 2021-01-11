c2e2b29e6bec32377100f3d565cb428b2fec30bb
Shawn Yu <shawnyudesign@gmail.com>
2021-01-11 12:54:51 -0500

    database

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 
 <?php
 // require './query.php';
- $server = 'localhost';
- $user='root'; 
- $pass='';
- $database='colorpiano';
+$server = 'j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$user='dpfc2uqv95ahytd1'; 
+$pass='n61qr185mcispxk3 ';
+$database='vrkysse652pm2xvs';
    
  $con = mysqli_connect($server, $user, $pass, $database)
   or die ("connection Failed");
